7057bb90ef505650ccec03746e10f4f1b9337c93
Marvin <marv.ris@gmail.com>
2024-01-18 15:59:37 +0100

    1. Version Infrarot Sensor

--- software/sensor_car.py
+++ software/sensor_car.py
@@ -1,69 +1,96 @@
-###
-# Infrarod Sensor zu Steering angle
-###
 from basisklassen import  *
-from basecar import *
-
 import numpy as np
 
-class SensorCar():
+irm = Infrared()
 
-    # Initialisierung der Klasse SensorCar.
-    def __init__(self):
-        # Infrared Klasse einlesen:
-        self.ir = Infrared()
-
-    @property
-    def ir_analog(self):
-        # Analogwerte der 5 IR-Sensoren auslesen mit Durschnitt aus 10 Messungen
-        # Ausgabe als Liste
-        self._ir_analog = (self.ir.get_average(10))
-        return self._ir_analog
+print("Ausgabe Messwerte:")
+for i in range(5):
+    data = irm.get_average(10)
+    print('{} : {}'.format(i, data))
+    time.sleep(.2)
     
-    def ir_normalize(arr, t_min, t_max):
-        # IR Messung auf eine range t_min und t_max normieren
-        # Aufruf in ir_to_steering
-        ir_normalized = []
-        diff = t_max - t_min
-        diff_arr = max(arr) - min(arr)
-        for i in arr:
-            temp = (((i - min(arr))*diff)/diff_arr) + t_min
-            ir_normalized.append(temp)
-            return ir_normalized
-      
-    def ir_to_steering(self):
-        # Lookup Table steering angle to IR-value
-        # np.arange(start=-10, stop=10, step=0.5)
-       
-        self._ir_to_sa = {
-            -2: -45,
-            -1.5: -33.75,
-            -1: -22.5,
-            -0.5: -11.25,
-            0: 0,
-            0.5: 11.25,
-            1: 22.5,
-            1.5: 33.75,
-            2: 45,
-            }
-        # IR Analog-Werte als Array:
-        ir_messwerte = np.array(self.ir_analog)
-                
-        # auf Max. Wert normieren ("prozentual")
-        ir_normiert = ir_messwerte / np.max(ir_messwerte) * 100
-        
-        # auf Bereich der Übertragungsfunktion (umwandeln:
-        ir_range_to_normalize = (-2,2)
-        
-        # Funktion zum normilisieren aufrufen:
-        ir_normal = ir_normalize(ir_normiert,
-                                 ir_range_to_normalize[0],
-                                 ir_range_to_normalize[1])
-                
-        # Die summe der 5 analogen Messwerte müsste die Lager der Linie sein
-        # Bei Summe 0 sollte Linie mittig sein
-        ir_summe = np.sum(ir_normal)
-        
-        # Lenkwinkel auf Basis der Übertragungsfunktion:
-        self._ir_steering_angle = self._ir_to_sa.get(ir_summe)
-        return self._ir_steering_angle
+#-------------------------
+# Benutzer Eingabe:
+#-------------------------
+while True:
+    input("Sensoren auf homogenen Untergrund platzieren und Taste Drücken...")
+    tmpcalib = irm.get_average(10)
+    print("Messergebnis:", tmpcalib)
+    user_in = input("Ergebsnis verwenden? (j/n/q)")
+    if user_in == "n":
+            print("Fahre fort...")
+            break
+    elif user_in == "j":
+        # Homogener Untergrund  - Abweichung zum Mittelwert = Kalibrier-Werte
+        messung = np.array(tmpcalib)
+        # Prozentuale Verrechnung ginge auch:
+        ir_calib = messung.mean() / messung
+        print("kalibrier0:",ir_calib)
+        # Nehmen jetzt den Mittelwert und subtrahieren diesen später
+        ir_calib = messung.mean()
+        print("kalibrier1:",ir_calib)
+        ir_calib = ir_calib.round(4)
+        print("kalibrier2:",ir_calib)
+        data = {}
+        try:
+            with open("config.json", "r") as f:
+                data = json.load(f)
+        except:
+            print("Read File error ")
+        data["ir_calib"] = ir_calib.tolist()
+        try:
+            with open("config.json", "w") as f:
+                json.dump(data, f)
+        except:
+            print("Write File error")
+            break
+        break
+    else:
+        print("Abbruch durch , fahre fort...")
+        break
+    
+#-------------------------
+# Kalirbrier-Werte aus Config lesen:
+#-------------------------
+print("Lese Kalibrier-Werte aus Config:")
+with open("config.json", "r") as f:
+    data = json.load(f)
+    ir_calib = data.get("ir_calib")
+    if ir_calib != None:
+        ir_calib = ir_calib
+    else:
+        ir_calib = [1, 1, 1, 1, 1]
+print("Kalibrierwert:", ir_calib)
+
+#-------------------------
+# Ausgabe der aktuellen IR Messwerte:
+#-------------------------
+iraverage = irm.get_average(3)
+print("Average Wert ohne Kalibrierung:")  
+print(iraverage)  
+
+#-------------------------
+# Ausgabe der kalibrierten IR Messwerte:
+#-------------------------
+# Kalibrierung verrechnung (Abweichung zum Mittelwert)
+ir_messwerte = ((irm.get_average(3) - np.array(ir_calib)).round(2).tolist())
+print("Avg Werte mit Kalirbrierung - Array ir_messwerte:")
+print(ir_messwerte)
+
+# -----------------
+# METHODE 1 über Minimum im Array
+# dort wo das Minimum ist = Position der Schw. Linie
+# -----------------
+print("Position der Spur (Minimum im Array:):")
+location_min_IR_array_value = ir_messwerte.index(min(ir_messwerte))
+print(location_min_IR_array_value)
+
+IR_to_steering_x = [0, 1, 2, 3, 4]
+IR_to_steering_y = [-45, -22.5, 0, 22.5, 45]
+
+# steering angle aus look up table interpolieren:
+
+steering_angle_interp = np.interp(location_min_IR_array_value, IR_to_steering_x, IR_to_steering_y)
+print("Interpolierter steering angle")
+print(steering_angle_interp)
+
