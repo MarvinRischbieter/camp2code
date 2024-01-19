42ef81620d5ec0f1a2b3c29296f89ce7e63b323d
ericc2c <ericc2c@spicysoda.com>
2024-01-19 02:14:26 +0100

    Update

--- software/sonic_car.py
+++ software/sonic_car.py
@@ -79,27 +79,3 @@ class RecordingThread(threading.Thread):
                 fp.write(';'.join(row_data))
                 fp.write('\n')
         self._lock_file.release()
-
-
-def road_test():
-    sc = SonicCar()
-
-    print(f'Recording start')
-    recording_thread = RecordingThread(sc)
-    recording_thread.start()
-
-    # Drive
-    sc.drive_with_params(Speed.MIN_SPEED.value, Direction.FORWARD.value, Angle.STRAIGHT_AHEAD.value)
-    time.sleep(3)
-
-    # Stop
-    sc.drive_with_params(Speed.STOP_SPEED.value, Direction.STANDSTILL.value, Angle.STRAIGHT_AHEAD.value)
-    time.sleep(1)
-
-    print(f'Recording stop')
-    recording_thread.stop_record()
-    recording_thread.join()
-
-
-if __name__ == '__main__':
-    road_test()
