dc8637ec5d6b7140ec13c2707141a39c1a2eeb96
Tobias <tob.wie@t-online.de>
2024-01-22 11:44:45 +0100

    erste version von parcour 7 line mit hindernis

---
+++ software/fahrparcours7.py
@@ -0,0 +1,44 @@
+from sensor_car import  *
+from record import *
+
+def follow_line_with_obstacle(ir, min_distance=5, max_distance=50, speed_min=20, speed_max=30 ):
+    
+    t = RecordingThread(ir)
+    t.start()
+    
+    ir.ir_calibriation()
+    messung = ir.get_ir_messung()
+    winkel = ir.get_steering_angle(messung)
+    print(f"Messung: {messung}")
+    print(f"winkel: {winkel}")
+    print(f"Wert der Kalibrierung: {ir.get_calibration()}")
+    ir.steering_angle = 90
+    p= input("Bitte Fzg auf die Linien setzen und Taste drücken") 
+         
+    parcour = True
+    speed = speed_max
+    
+    while parcour:
+        
+        try:
+            distance_to_obstacle = ir.get_distance_to_obstacle()
+            if distance_to_obstacle <= min_distance:
+               print("Hindernis erkannt! Stopping...")
+               ir.stop()
+               parcour = False
+               break
+            if distance_to_obstacle <= max_distance or distance_to_obstacle == 1000 :
+                print("Hindernis erkannt! langsamer...")
+                speed = speed_min
+            else:
+                speed = speed_max
+                
+            ir.drive(speed, 1)
+            messung = ir.get_ir_messung()
+            ir.steering_angle = ir.get_steering_angle(messung)
+        except KeyboardInterrupt:
+            parcour=False  
+    ir.stop()
+    ir.steering_angle = 90 # Lenkung wieder gerade
+    t.stop_record()
+    t.join()
--- software/test.py
+++ software/test.py
@@ -7,6 +7,7 @@ from fahrparcours2 import *
 from fahrparcours3 import * 
 from fahrparcours4 import *
 from fahrparcours5 import *
+from fahrparcours7 import *
 import os
 import traceback
 import time
@@ -21,10 +22,10 @@ def main():
     ausführen=True
     while ausführen:
         try:
-            print("Fahrprogramme:\n   1: diverse BaseCar\n   2: Test SonicCar\n   3: Test Parcours 1\n   4: Test Parcours 2\n   5: Test Parcours 3\n   6: Test Parcours 4 \n   7: Test Parcours 5 \n   8: Abbruch ")
+            print("Fahrprogramme:\n   1: Parcours 1: vorwärts rückwärts\n   2: Parcours 2: Kreisfahrt\n   3: Parcours 3: Vorwärts bis Hinderniss \n   4: Parcours 4: Erkundungstour\n   5: Parcours 5: Linenverfolgung \n   6: Parcours 6: Linenverfolgung mit Hindernisserk.  \n   7: Abbruch ")
             p= input("Bitte wählen Sie ein Fahrprogramm : ")
             programm = int(p)
-            if programm <= 0 or programm > 8:
+            if programm <= 0 or programm > 7:
                 print("Bitte 5einen nummerischen Wert im Wertebereich eingeben")
                 time.sleep(2)
                 continue
@@ -35,71 +36,32 @@ def main():
         else:
 
             if programm == 1:
-                #################### Test Base Car #########################     
-                bc = BaseCar()
-                bc.steering_angle = 45
-                print (bc.steering_angle)
-                time.sleep(2)
-                bc.steering_angle = 135
-                print (bc.steering_angle)
-                time.sleep(2)
-                bc.steering_angle = 90
-                print (bc.steering_angle)
-                time.sleep(2)
-                bc.speed = 50
-                print (f"Geschwindigkeit : {bc.speed}")
-                time.sleep(2)
-                bc.speed = 0
-                time.sleep(2)
-                bc.drive(50, 1)
-                time.sleep(2)
-                bc.stop()
-                time.sleep(2)
-                bc.drive(300, -1)
-                time.sleep(2)
-                bc.stop()
-                bc.drive(-300, -1)
-                time.sleep(2)
-                bc.stop()
-            elif programm == 2:
-                ##################### Test Sonic Car #########################
-                sc = SonicCar()
-                sc.steering_angle = 45
-                print (sc.steering_angle)
-                time.sleep(2)
-                sc.steering_angle = 135
-                print (sc.steering_angle)
-                time.sleep(2)
-                sc.steering_angle = 90
-                print (sc.steering_angle)
-                time.sleep(2)
-                #sc.make_measures() # 100 Mal
-                #distance_obs = sc.get_distance_to_obstacle() # 1 Mal
-                #print(f'distance: {distance_obs}')
-            elif programm == 3:
-                ###################### Test parcours1 #########################
                 bc = BaseCar()
                 fahrparcours1(bc)
                 time.sleep(2)
-            elif programm == 4:
-                ###################### Test parcours2 #########################
+            elif programm == 2:
                 bc = BaseCar()
                 fahrparcours2(bc)
                 time.sleep(2)
-            elif programm == 5:
-                ##################### Test parcours3 #########################
+            elif programm == 3:
+                ###################### Test parcours1 #########################
                 sc = SonicCar()
                 drive_until_obstacle(sc)
                 time.sleep(2)
-            elif programm == 6:
-                ##################### Test parcours4 #########################
+            elif programm == 4:
+                ###################### Test parcours2 #########################
                 sc = SonicCar()
                 fahrparcours_4(sc)
                 time.sleep(2)
-            elif programm == 7:
-                ##################### Test parcours4 #########################
+            elif programm == 5:
+                ##################### Test parcours3 #########################
                 ir = SensorCar()
                 follow_line(ir)
+                time.sleep(2)
+            elif programm == 6:
+                ##################### Test parcours4 #########################
+                ir = SensorCar()
+                follow_line_with_obstacle(ir)
                 time.sleep(2)
             else:
                 print("Goodby")
