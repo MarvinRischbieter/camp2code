0d67c7561546c00e9fe5ab2b87a1b014c0a50db7
Tobias <tob.wie@t-online.de>
2024-01-22 15:09:54 +0100

    linien verfolgung stopt nun bei keiner Linie

--- software/fahrparcours5.py
+++ software/fahrparcours5.py
@@ -1,7 +1,7 @@
 from sensor_car import  *
 from record import *
 
-def follow_line(ir):
+def follow_line(ir, linie_Schwellwert = -4, anzahl_linien_ende = 5 ):
     
     t = RecordingThread(ir)
     t.start()
@@ -16,11 +16,29 @@ def follow_line(ir):
     p= input("Bitte Fzg auf die Linien setzen und Taste drücken") 
          
     parcour = True
-    while parcour:
+    linie = True
+    keine_linie = 0
+    while parcour and linie:
         try: 
             ir.drive(30, 1)
             messung = ir.get_ir_messung()
+            print (messung)
             ir.steering_angle = ir.get_steering_angle(messung)
+            
+            linie = False
+            for i in messung :
+                #print (f"Sensorwert: {i}")
+                if i <= linie_Schwellwert :
+                    linie = True
+                    keine_linie = 0
+            if  not linie:
+                keine_linie +=1
+                linie = True
+                print("keine Linie")
+            if keine_linie >= anzahl_linien_ende:
+                linie = False
+                print("Linien Ende erreicht")
+                
         except KeyboardInterrupt:
             parcour=False  
     ir.stop()
