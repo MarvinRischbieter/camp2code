e464b79d1462416cf0ca97f30896bb75a7000fc9
Tobias <tob.wie@t-online.de>
2024-01-25 14:54:25 +0100

    auswertung.py: radiobutton für fahrdaten eingefügt

--- software/auswertung.py
+++ software/auswertung.py
@@ -21,7 +21,8 @@ def csv_dateien_einlesen():
                 csv_dateien.append(datei)
     return csv_dateien
 
-c = csv_dateien_einlesen()
+dateiliste = csv_dateien_einlesen()
+#parameterlist=['Speed','Angle']
 
 #################################################################
 # Definition der cards (1: Geschwindigkeit, 2 mittlere Gesch., ...)
@@ -85,7 +86,11 @@ app.layout = html.Div(
     children=[
        
         html.H1(children='Auswertung der Fahrten'),
-        dcc.Dropdown( c, c[0], id="Dateiauswahl", style ={'backgroundColor': "#ffffff",'color': '#660066'}  ),   
+        dcc.Dropdown( dateiliste, dateiliste[0], id="Dateiauswahl", style ={'backgroundColor': "#ffffff",'color': '#660066'}  ),   
+        dcc.RadioItems(options= ['Speed', 'Angle','Direction','Distance','Infra'], value='Speed',
+                id='fahrdaten',
+                inline=True
+            ),
         dbc.Row(
             [
             dbc.Col([card1], width="auto"),
@@ -96,7 +101,7 @@ app.layout = html.Div(
             ] 
         ),        
         dcc.Graph(id='line_plot'), # Graph Basis
-        dcc.Graph(id='line_plot_dist'), # Graph-Komponente 
+        #dcc.Graph(id='line_plot_dist'), # Graph-Komponente 
     ]
 )
 
@@ -110,11 +115,20 @@ app.layout = html.Div(
               Output(component_id='v_min', component_property="children" ),
               Output(component_id='v_time', component_property="children" ),
               Output(component_id='v_dist', component_property="children" ),
-              Input(component_id='Dateiauswahl', component_property='value'))
-def graph_update(value_of_input_component):
+              Input(component_id='Dateiauswahl', component_property='value'),
+              Input(component_id='fahrdaten', component_property='value'),)
+def graph_update(value_of_input_component,fahrdaten):
+    #print(value_of_input_component)
+    #print(fahrdaten)
     df= pd.read_csv(value_of_input_component,';')  # Dataframe aus der CSV erstellen
+    
     Timestamp= df["Time"]
-    print(Timestamp)
+    Time = []
+    for t in Timestamp:
+        Time.append(t[11:-7])
+    Timeseries = pd.Series(Time) 
+    df["Time2"] = Timeseries
+    
     mid = df["Speed"].mean()
     max = df["Speed"].max()
     min = df["Speed"].min()
@@ -140,17 +154,23 @@ def graph_update(value_of_input_component):
     # Gesamtdistanz und Fahzeit ermitteln
     v_dist = f"{d2} cm"
     v_time = f"{time_dif.total_seconds()} sec."   
-    fig = px.line(df, x='Time', y=['Speed','Angle','Direction',])
+    fig = px.line(df, x='Time2', y=[fahrdaten])
     
     return fig,v_max,v_mid,v_min,v_time,v_dist
-
+'''
 @app.callback(Output(component_id='line_plot_dist', component_property='figure'),
               Input(component_id='Dateiauswahl', component_property='value'))
 def graph_update(value_of_input_component):
     df= pd.read_csv(value_of_input_component,';')  # Dataframe aus der CSV erstellen
-    fig2 = px.line(df, x='Time', y=['Distance'])
+    Timestamp= df["Time"]
+    Time = []
+    for t in Timestamp:
+        Time.append(t[11:-7])
+    Timeseries = pd.Series(Time) 
+    df["Time2"] = Timeseries
+    fig2 = px.line(df, x='Time2', y=['Distance'])
     return fig2
-
+'''
 
 if __name__ == '__main__':
     app.run_server(debug=True)
